b5315479712604c158f2c4d2cb2f3211f2916ce0
Noah-Lee Dierna <nl.dierna@evs.com>
2022-09-26 17:00:26 +0200

    set up ptoject

---
+++ seller/src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css';
+
+export default function NavBar() {
+  return (
+        <>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        </>
+    )
+}
