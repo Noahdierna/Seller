7ce8d15a9a07d6a867fc5dfb284ad74fb8d5adcf
Noah-Lee Dierna <nl.dierna@evs.com>
2022-09-27 11:54:38 +0200

    creating the interface

--- seller/src/App.js
+++ seller/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar'
+import Home from './Containers/Home/Home'
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
--- seller/src/Components/NavBar/NavBar.js
+++ seller/src/Components/NavBar/NavBar.js
@@ -3,12 +3,13 @@ import './NavBar.css';
 
 export default function NavBar() {
   return (
-        <>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Service</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-        </>
+        <div className="navBar">
+            <ul>
+                <li>Home</li>
+                <li>Edit</li>
+                <li>contact</li>
+            </ul>
+        </div>
+      
     )
 }
